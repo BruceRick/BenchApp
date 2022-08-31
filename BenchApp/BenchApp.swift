63f26bae568a60f17bc7fc07be028f2a680ae28b
Bruce Rick <brucerickti@gmail.com>
2022-08-30 21:17:14 -0400

    Initial commit

---
+++ BenchApp/BenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchApp.swift
+//  BenchApp
+//
+//  Created by Bruce Rick on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct BenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ BenchApp/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  BenchApp
+//
+//  Created by Bruce Rick on 2022-08-30.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
